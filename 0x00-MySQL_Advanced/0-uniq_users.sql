2ff266ca242ae21c5ebe8cc1a17fbdb8a10daafe
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-10-17 00:26:59 +0100

    initial commit with README.md and task 0

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,6 @@
+-- Creates a table users
+-- users table created with attributes defined
+CREATE TABLE IF NOT EXISTS users (
+		id INT AUTO-INCREMENT NOT NULL PRIMARY KEY,
+		email VARCHAR(255) NOT NULL UNIQUE
+		name VARCHAR(255) );
---
+++ README.md
@@ -0,0 +1,6 @@
+## ALX Backend Storage
+
+All projects on backend storage lives here
+
+Directory list and content brief:
+- 0x00-MySQL_Advanced: this contains tasks on advanced SQL queries, creating views and functions
