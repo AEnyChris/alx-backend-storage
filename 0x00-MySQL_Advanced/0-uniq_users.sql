7eff73d79f5d2ef5d4da4b9f462f12a02e909f19
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-10-17 00:34:20 +0100

    corrections

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,6 @@
 -- Creates a table users
 -- users table created with attributes defined
-DROP TABLE IF EXISTS;
+DROP TABLE IF EXISTS users;
 CREATE TABLE IF NOT EXISTS users (
 		id INT AUTO-INCREMENT NOT NULL PRIMARY KEY,
 		email VARCHAR(255) NOT NULL UNIQUE,
