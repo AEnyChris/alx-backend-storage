98ffd27fc65151a991846751685ae05129f5799d
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-10-17 23:20:09 +0100

    added task 5

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,12 @@
+-- create trigger to validate email
+DROP TRIGGER IF EXISTS validate_email;
+DELIMITER //
+CREATE TRIGGER validate_email
+BEFORE UPDATE ON users
+FOR EACH ROW
+		BEGIN
+				IF OLD.email != NEW.email THEN
+						SET NEW.valid_email = 0;
+				END IF;
+		END;//
+DELIMITER ;
