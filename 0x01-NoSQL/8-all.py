9b7a396d96fe530a642f73989c7ce5e857ca6a26
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-10-22 21:19:08 +0100

    remove the if __name__

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 """list all docs in a mongodb collection"""
 
-if __name__ == '__main__':
-    def list_all(mongo_collection):
-        for doc in  mongo_collection.find():
-            yield doc
+def list_all(mongo_collection):
+    for doc in  mongo_collection.find():
+        yield doc
