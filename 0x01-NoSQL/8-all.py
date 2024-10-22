a62d99b3731f8932f42edcad41ed8a1cb1ddfb74
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-10-22 21:24:42 +0100

    correction: pycodestyle

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """list all docs in a mongodb collection"""
 
+
 def list_all(mongo_collection):
-    for doc in  mongo_collection.find():
+    for doc in mongo_collection.find():
         yield doc
