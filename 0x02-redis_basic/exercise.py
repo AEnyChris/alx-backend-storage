c65803086ffe18a4c9a944f4de5d19bb12c3fa41
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-10-23 23:17:47 +0100

    added task 1: define get, get_str and get_int

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,11 +2,11 @@
 """Define class Cache for data storage"""
 from uuid import uuid4
 import redis
-from typing import Union
+from typing import Union, Callable
 
 
 class Cache:
-    """the class Cache for storing 
+    """the class Cache for storing
     data in a Redis data storage.
     """
     def __init__(self) -> None:
@@ -16,9 +16,31 @@ class Cache:
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
-        Stores a value `data` in a Redis data 
+        Stores a value `data` in a Redis data
         storage and returns the key.
         """
         key = str(uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(
+            self,
+            key: str,
+            fn: Callable = None,
+            ) -> Union[str, bytes, int, float]:
+        """
+        retrieves and convert data from
+        redis to desired format using a callable fn
+        """
+        data = self._redis.get(key)
+        return fn(data) if fn is not None else data
+
+    def get_str(self, key: str) -> str:
+        """parametarize data to string from a redis storage"""
+        res = self.get(key, lambda x: x.decode('utf-8'))
+        return res
+
+    def get_int(self, key: str) -> int:
+        """parametarize data to integer from a redis storage"""
+        res = self.get(key, lambda x: int(x))
+        return res
