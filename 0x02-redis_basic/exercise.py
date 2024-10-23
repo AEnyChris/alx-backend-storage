fd3ac720b6dfc1c5ba02a7ac9453ab3ba26fd208
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-10-23 22:23:13 +0100

    correction on type annotation of method store

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
 """Defines the class Cache"""
 from uuid import uuid4
-from typing import Any
+from typing import Union
 import redis
 
 
 class Cache:
-    """The Cache class for storing string to redis"""
+    """The Cache class for storing data to redis"""
     def __init__(self) -> None:
         """initializes a Cache instance"""
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: Any) -> str:
+    def store(self, data: Union[str, int, float, bytes]) -> str:
         """
         stores data into redis with a random key (uuid4)
         and returns the key
