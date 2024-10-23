989e34e707ad83f9af60527478b0ca18f1442a6e
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-10-23 22:39:53 +0100

    revised

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -6,17 +6,19 @@ from typing import Union
 
 
 class Cache:
-    '''the class Cache for storing data in a Redis data storage.
-    '''
+    """the class Cache for storing 
+    data in a Redis data storage.
+    """
     def __init__(self) -> None:
-        '''Initializes a Cache instance.
-        '''
+        """Initializes a Cache instance"""
         self._redis = redis.Redis()
         self._redis.flushdb(True)
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        '''Stores a value `data` in a Redis data storage and returns the key.
-        '''
+        """
+        Stores a value `data` in a Redis data 
+        storage and returns the key.
+        """
         key = str(uuid4())
         self._redis.set(key, data)
         return key
