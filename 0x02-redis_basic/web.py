46c4b27689b5e4a811d02ec54ffb6401d4a7f6fb
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-10-24 02:07:31 +0100

    added task 5: url tracking

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -9,8 +9,23 @@ from typing import Callable
 
 redis = redis.Redis()
 
-
-
+def data_cacher(method: Callable) -> Callable:
+    '''Caches the output of fetched data.
+    '''
+    @wraps(method)
+    def invoker(url) -> str:
+        '''The wrapper function for caching the output.
+        '''
+        redis_store.incr(f'count:{url}')
+        result = redis_store.get(f'result:{url}')
+        if result:
+            return result.decode('utf-8')
+        result = method(url)
+        redis_store.set(f'count:{url}', 0)
+        redis_store.setex(f'result:{url}', 10, result)
+        return result
+    return invoker
+'''
 def url_tracker(method: Callable) -> Callable:
     """
     Decorator to track get request on a url.
@@ -29,9 +44,9 @@ def url_tracker(method: Callable) -> Callable:
         redis.setex(f'response:{url}', 10, response)
         return response
     return trigger
+'''
 
-
-@url_tracker
+@data_cacher
 def get_page(url: str) -> str:
     """
     gets the content of a url, caches it and tracks how
