9e99a6f7339581e7cfd126f7a25545741fd8af6a
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-10-24 02:12:55 +0100

    new corrections

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -8,6 +8,8 @@ from typing import Callable
 
 
 redis_store = redis.Redis()
+'''The module-level Redis instance.
+'''
 
 
 def data_cacher(method: Callable) -> Callable:
@@ -28,32 +30,9 @@ def data_cacher(method: Callable) -> Callable:
     return invoker
 
 
-'''
-def url_tracker(method: Callable) -> Callable:
-    """
-    Decorator to track get request on a url.
-    Counts requests and content is cached for 10 seconds
-    """
-    @wraps(method)
-    def trigger(url) -> str:
-        """The wrapper function.
-        """
-        redis.incr(f'count:{url}')
-        response = redis.get(f'response:{url}')
-        if response:
-            return response.decode('utf-8')
-        response = method(url)
-        redis.set(f'count:{url}', 0)
-        redis.setex(f'response:{url}', 10, response)
-        return response
-    return trigger
-'''
-
-
 @data_cacher
 def get_page(url: str) -> str:
-    """
-    gets the content of a url, caches it and tracks how
-    many times it was requested
-    """
+    '''Returns the content of a URL after caching the request's response,
+    and tracking the request.
+    '''
     return requests.get(url).text
